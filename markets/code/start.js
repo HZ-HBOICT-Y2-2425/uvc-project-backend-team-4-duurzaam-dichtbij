de99d7df96845d3f8dbc45f52001903591d57bea
ThianWarringtonHZ <warr0006@hz.nl>
2024-11-28 15:54:35 +0100

    Made markets microservice available from gateway

--- markets/code/start.js
+++ markets/code/start.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 
-app.set('port', process.env.PORT || 3011);
+app.set('port', process.env.PORT || 3012);
 const server = app.listen(app.get('port'), () => {
   console.log(`🍿 Express running → PORT ${server.address().port}`);
 });
