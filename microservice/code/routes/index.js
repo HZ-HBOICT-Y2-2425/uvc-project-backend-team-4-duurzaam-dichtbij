92612e3014c72b9800691835d50a35753ab506ee
Dirk <dijk0166@hz.nl>
2024-12-11 14:49:39 +0100

    Updated to match frontend

--- microservice/code/routes/index.js
+++ microservice/code/routes/index.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import { getRecipes, getRecipeById, getRecipeIngredients, getRecipeInstructions } from '../controllers/recipeController.js';
+
 const router = express.Router();
 
-// routes
-router.get('/', (req, res, next) => {
+// Basisroute
+router.get('/', (req, res) => {
   res.json('hi');
 });
+
+// Routes zonder cacheMiddleware
 router.get('/recipes', getRecipes);
 router.get('/recipes/:id', getRecipeById);
 router.get("/recipes/:id/instructions", getRecipeInstructions);
 router.get("/recipes/:id/ingredients", getRecipeIngredients);
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 export default router;
